bd1b30477d6ff1efba6160ce4b309cd3e75ad77a
Joao Sgreccia <jmarcelos@gmail.com>
2016-09-29 11:02:53 -0300

    Reading products urls

--- coles/coles/spiders/coles.py
+++ coles/coles/spiders/coles.py
@@ -4,17 +4,20 @@ import scrapy
 class ColesSpider(scrapy.Spider):
     name = 'myspider'
 
-    def __init__(self, url=None, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super(ColesSpider, self).__init__(*args, **kwargs)
-        self.start_urls = [url]
+        self.urls = [kwargs.get('url')]
 
     def start_requests(self):
         for url in self.urls:
             yield scrapy.Request(url=url, callback=self.get_homepages_url)
-    
+   
+
+    def get_products_url(self, urls):
+        return [u'http://shop.coles.com.au/online/national/coca-cola-soft-drink-coke-375ml-cans-7365777p', u'http://shop.coles.com.au/online/national/coca-cola-soft-drink-coke-zero-chilled']
+
 
     def get_homepages_url(self, response):
         url_rules = '//ul[@id="subnav"]/li/a/@href'
         urls = response.xpath(url_rules).extract()
-        
         return urls 
--- coles/coles/spiders/tests.py
+++ coles/coles/spiders/tests.py
@@ -12,6 +12,15 @@ class TestColesSpider(unittest.TestCase):
         self.assertEquals(urls[0], u'http://shop.coles.com.au/online/national/drinks/soft-drinks-3314551')
         self.assertEquals(urls[1], u'http://shop.coles.com.au/online/national/drinks/cordials')
 
+    def test_get_products(self):
+        coles = ColesSpider('http://shop.coles.com.au/online/national/drinks')
+        response = coles_response('drinks.html')
+        urls = coles.get_homepages_url(response)
+        products_urls = coles.get_products_url(urls)
+        self.assertEquals(len(products_urls), 2)
+        self.assertEquals(products_urls[0], u'http://shop.coles.com.au/online/national/coca-cola-soft-drink-coke-375ml-cans-7365777p',)
+        self.assertEquals(products_urls[1], u'http://shop.coles.com.au/online/national/coca-cola-soft-drink-coke-zero-chilled')
+
 if __name__ == '__main__':
     unittest.main()
 
