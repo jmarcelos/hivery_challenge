242a2e368d54cfb89c399fe79d9688f57bb49ff0
Joao Sgreccia <jmarcelos@gmail.com>
2016-10-07 06:51:05 -0300

    refactor e decoupling tests from webdriver

--- scrapper/coles/spiders/coles.py
+++ scrapper/coles/spiders/coles.py
@@ -1,6 +1,7 @@
 import os
 import re
 import json
+import logging
 import scrapy
 from selenium import webdriver
 from scrapper.coles.items import Product, Region, PriceRegion
@@ -9,62 +10,105 @@ from scrapy.utils.project import get_project_settings
 class ColesSpider(scrapy.Spider):
 
     name = 'coles_general_spider'
-    SEARCH_TOTAL_VALUES = 20
+    _search_total_values = 20
     regions = []
+    _regex_double_quote = r'"(.*?)"'
+    _regex_sigle_quote = r"'(.*?)'"
+    _url_region = u'https://shop.coles.com.au/online/national/COLAjaxAutoSuggestCmd?searchTerm={0}&expectedType=json-comment-filtered&serviceId=COLAjaxAutoSuggestCmd'
+    _search_url = u'https://shop.coles.com.au/online/national/drinks/ColesCategoryView?orderBy={0}&productView=list&beginIndex={1}&pageSize={2}&catalogId={3}&storeId=10601&categoryId={4}&localisationState=2&serviceId=ColesCategoryView&langId=-1'
+    _url_price_region = u'https://shop.coles.com.au/online/COLLocalisationControllerCmd'
+    _params_price_region = u"redirectUrl={0}&suburbPostCodeId={1}&serviceType={2}&externalffmcId={3}&state={4}&score={5}&collectionpoint={6}&suburb={7}&zoneid={8}&postcode={9}&longitude={10}&latitude={11}&storeId=10601"
+
 
     def __init__(self, *args, **kwargs):
         super(ColesSpider, self).__init__(*args, **kwargs)
+        
         self.start_urls = [kwargs.get('url')]
         dirty_postcodes = kwargs.get('postcode') or ""
         self.postcodes = dirty_postcodes.split(',')
+        
+        if self.start_urls is None or self.start_urls is "":
+            raise ValueError("start url was not defined")
 
     def start_requests(self):
 
         for postcode in self.postcodes:
-            url = u'https://shop.coles.com.au/online/national/COLAjaxAutoSuggestCmd?searchTerm={0}&expectedType=json-comment-filtered&serviceId=COLAjaxAutoSuggestCmd'.format(postcode)
-            yield scrapy.Request(url=url, callback=self.get_region)
+            url = self._url_region.format(postcode)
+            yield scrapy.Request(url=url, callback=self._get_region)
 
         for url in self.start_urls:
             yield scrapy.Request(url=url, callback=self.yield_get_homepages_url)
 
-    def get_product_detail(self, response):
-        dirty_name = response.xpath('//div[@id="FBLikeDiv"]/@data-social').extract()[0] 
-        regex = r"'(.*?)'"
-        name = re.findall(regex, dirty_name)[2] 
-        brand = response.xpath('//div[@class="brand"]/text()').extract()[0]
-        product_info = response.xpath('//div[@id="FBLikeDiv"]/@data-social').extract()[0]
-        redirect_url = response.xpath('//form [@action="COLLocalisationControllerCmd"]/input[@name="fullURLHttps"]/@value').extract()[0]
 
-        info_answer = response.xpath('//div[@class="plain-box product-detail-left"]/p/text()').extract()
-        info_question = response.xpath('//div[@class="plain-box product-detail-left"]/h2/text()').extract()
-        info = dict(zip(info_answer, info_question))
 
-        description = re.findall(regex, product_info)[3] or "Not Available"
-        product_img = re.findall(regex, product_info)[0]
-        url_friendly_name_dirty = re.findall(regex, product_info)[1]
-        url_friendly_name = url_friendly_name_dirty.split('/')[-1]
-        social_data = response.xpath('//div[@id="reviewsCommentsDiv"]/@data-social').extract()[0]
-        product_id = re.findall(regex, social_data)[0]
-
-        dirty_data = response.xpath('//div[@class="prodtile"]/form/div/@data-refresh').extract()[0]
-        data = re.sub('\s+', '', dirty_data)
-        regex = r'"(.*?)"'
-        price_type = re.findall(regex, data)[9]
-
-        general_price = response.xpath('//div[@class="purchasing"]/div[@class="price"]/text()').extract()[0]
-
-        product = Product(name=name.rstrip(), brand=brand, description=description, info=info,
-                url_friendly_name=url_friendly_name, product_img=product_img, 
-                id=product_id, general_price=general_price,redirect_url=redirect_url,
-                price_type=price_type, prices_region=[], has_price_difference=False)
-
-        for region in self.regions:
-            product = self.get_product_price_region(region, product)
+    def yield_get_search_page(self, response):
+        search_url = self._get_search_url(response)
         
-        return product
+        yield scrapy.Request(url=search_url, callback=self.yield_get_products_url)
 
+    def yield_get_products_url(self, response):
+        products_url, search_url = self.get_products_url(response)
+        for url in products_url:
+            yield scrapy.Request(url=url, callback=self._get_product_detail)
+    
+        if search_url:
+            yield scrapy.Request(url=search_url, callback=self.yield_get_products_url)
 
-    def get_region(self, response):
+    def yield_get_homepages_url(self, response):
+        urls = self._get_homepages_url(response)
+        
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.yield_get_search_page)
+
+    def _get_product_detail(self, response):
+        try:
+            dirty_name = response.xpath('//div[@id="FBLikeDiv"]/@data-social').extract()[0] 
+            regex = r"'(.*?)'"
+            name = re.findall(regex, dirty_name)[2] 
+            brand = response.xpath('//div[@class="brand"]/text()').extract()[0]
+            product_info = response.xpath('//div[@id="FBLikeDiv"]/@data-social').extract()[0]
+            redirect_url = response.xpath('//form [@action="COLLocalisationControllerCmd"]/input[@name="fullURLHttps"]/@value').extract()[0]
+
+            info_answer = response.xpath('//div[@class="plain-box product-detail-left"]/p/text()').extract()
+            info_question = response.xpath('//div[@class="plain-box product-detail-left"]/h2/text()').extract()
+            info = dict(zip(info_question, info_answer))
+
+            description = re.findall(regex, product_info)[3] or "Not Available"
+            product_img = re.findall(regex, product_info)[0]
+            url_friendly_name_dirty = re.findall(regex, product_info)[1]
+            url_friendly_name = url_friendly_name_dirty.split('/')[-1]
+            social_data = response.xpath('//div[@id="reviewsCommentsDiv"]/@data-social').extract()[0]
+            product_id = re.findall(regex, social_data)[0]
+
+            dirty_data = response.xpath('//div[@class="prodtile"]/form/div/@data-refresh').extract()[0]
+            data = re.sub('\s+', '', dirty_data)
+            regex = r'"(.*?)"'
+            price_type = re.findall(regex, data)[9]
+
+            general_price = response.xpath('//div[@class="purchasing"]/div[@class="price"]/text()').extract()[0]
+
+            product = Product(name=name.rstrip(), brand=brand, description=description, info=info,
+                    url_friendly_name=url_friendly_name, product_img=product_img, 
+                    id=product_id, general_price=general_price,redirect_url=redirect_url,
+                    price_type=price_type, prices_region=[], has_price_difference=False)
+
+            for region in self.regions:
+                product = self._get_product_price_region(region, product)
+            
+            return product
+        
+        except:
+            logging.debug("Not possible to crawl product under url {}".format(response.url))
+
+    def _extract_search_parameters(self, search_dirty_parameters):
+        order_by = re.findall(self._regex_sigle_quote, search_dirty_parameters)[2] or ""
+        begin_index = re.findall(self._regex_sigle_quote, search_dirty_parameters)[3] or ""
+        catalog_id = re.findall(self._regex_sigle_quote, search_dirty_parameters)[6]  or ""
+        category_id = re.findall(self._regex_sigle_quote, search_dirty_parameters)[7] or ""
+
+        return order_by, begin_index, catalog_id, category_id
+
+    def _get_region(self, response):
         dirty_json = re.sub('\s+', '', response.body)        
         regions_string = dirty_json[2:-2]
         regions = json.loads(regions_string)
@@ -88,80 +132,49 @@ class ColesSpider(scrapy.Spider):
    
         self.regions.append(region)
 
-    def yield_get_search_page(self, response):
-        search_url = self.get_search_url(response)
-        
-        yield scrapy.Request(url=search_url, callback=self.yield_get_products_url)
-
-
-    def get_search_url(self, response):
-        search_rules = '//div[@id="searchDisplay"]/@data-refresh'
-        search_dirty_parameters = response.xpath(search_rules).extract()[0]
-        order_by, begin_index, catalog_id, category_id = self.extract_search_parameters(search_dirty_parameters)
-        search_url = self.generate_search_url(order_by, begin_index, catalog_id, category_id)
-        return search_url 
-
-    def extract_search_parameters(self, search_dirty_parameters):
-        regex = r"'(.*?)'"
-        order_by = re.findall(regex, search_dirty_parameters)[2] or ""
-        begin_index = re.findall(regex, search_dirty_parameters)[3] or ""
-        catalog_id = re.findall(regex, search_dirty_parameters)[6]  or ""
-        category_id = re.findall(regex, search_dirty_parameters)[7] or ""
-
-        return order_by, begin_index, catalog_id, category_id
-
-
-    def generate_search_url(self, order_by, begin_index, catalogId, categoryId):
-        search_url = u'https://shop.coles.com.au/online/national/drinks/ColesCategoryView?orderBy={0}&productView=list&beginIndex={1}&pageSize={2}&catalogId={3}&storeId=10601&categoryId={4}&localisationState=2&serviceId=ColesCategoryView&langId=-1'.format(order_by, begin_index, self.SEARCH_TOTAL_VALUES, catalogId, categoryId)
-        
-        return search_url
-
-    def yield_get_products_url(self, response):
-        products_url, search_url = self.get_products_url(response)
-        for url in products_url:
-            yield scrapy.Request(url=url, callback=self.get_product_detail)
-    
-        if search_url:
-            yield scrapy.Request(url=search_url, callback=self.yield_get_products_url)
-
-
     def get_products_url(self, response):
         url_rules = '//div[@class="outer-prod prodtile"]/@data-refresh'
         products = response.xpath(url_rules).extract()
-        regex = r'"(.*?)"'
+        
         products_url = []
         for product in products:
-            url = re.findall(regex, product)[4]
+            url = re.findall(self._regex_double_quote, product)[4]
             products_url.append(url)
        
         search_url = None
         next_rules = '//li[@class="next"]'
-        has_next = len(response.xpath(next_rules).extract()) > 0 or False
-        if has_next:
-            search_rules = '//div[@id="searchDisplay"]/@data-refresh'
-            search_dirty_parameters_list = response.xpath(search_rules).extract()
-            if len(search_dirty_parameters_list) > 0:
-                search_dirty_parameters = search_dirty_parameters_list[0]
-                order_by, begin_index, catalog_id, category_id = self.extract_search_parameters(search_dirty_parameters)
-                search_url = self.generate_search_url(order_by, int(begin_index) + self.SEARCH_TOTAL_VALUES, catalog_id, category_id)
+        
+        if len(response.xpath(next_rules).extract()):
+           search_url = self._get_search_url(response)
         return products_url, search_url
 
-    def yield_get_homepages_url(self, response):
-        urls = self.get_homepages_url(response)
-        
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.yield_get_search_page)
+    def _get_search_url(self, response):
+        search_url = ''
+        search_rules = '//div[@id="searchDisplay"]/@data-refresh'
+        search_dirty_parameters_list = response.xpath(search_rules)
+        if len(search_dirty_parameters_list) > 0:
+            search_dirty_parameters = search_dirty_parameters_list.extract()[0]
+            order_by, begin_index, catalog_id, category_id = self._extract_search_parameters(search_dirty_parameters)
+            search_url = self._search_url.format(order_by, begin_index, self._search_total_values, catalog_id, category_id)
+    
+        return search_url 
+    
 
-    def get_homepages_url(self, response):
+    def _get_homepages_url(self, response):
         url_rules = '//ul[@id="subnav"]/li/a/@href'
         urls = response.xpath(url_rules).extract()
         return urls
 
-    def get_product_price_region(self, region, product):
-        url = self.get_price_per_region_url(region, product)
-        driver = self.get_driver()
+    def _get_price_region(self, url):
+        driver = self._get_driver()
         driver.get(url)
         price = driver.find_element_by_xpath('//strong[@class="product-price"]').text
+        
+        return price
+
+    def _get_product_price_region(self, region, product):
+        url = self._get_price_per_region_url(region, product)
+        price = self._get_price_region(url)
         
         #just update flag if there is no price difference
         if  not product['has_price_difference']:
@@ -171,20 +184,23 @@ class ColesSpider(scrapy.Spider):
         product['prices_region'].append(price_region)
         return product 
 
-    def get_price_per_region_url(self, region, product):
+    def _get_price_per_region_url(self, region, product):
         try:
-            url = 'https://shop.coles.com.au/online/COLLocalisationControllerCmd'
-            params = "redirectUrl={0}&suburbPostCodeId={1}&serviceType={2}&externalffmcId={3}&state={4}&score={5}&collectionpoint={6}&suburb={7}&zoneid={8}&postcode={9}&longitude={10}&latitude={11}&storeId=10601".format(product['redirect_url'], region['id'],
+            params = self._params_price_region.format(product['redirect_url'], region['id'],
                     region['service_type'], region['webstore_id'], region['state'], region['score'], region['collectionpoint'],
                     region['suburb'], region['zone_id'], region['postcode'], region['lon'], region['lat'])
-            final_url = url + '?' + params
+           
+            final_url = self._url_price_region + '?' + params
+            
             return final_url
+        
         except: 
              print 'Not possible to retrieve information from product {0} region {1}'.format(product, region)
 
-    def get_driver(self):
+    def _get_driver(self):
         chromedriver = get_project_settings()['CHROME_DRIVE_LOCATION']
         os.environ["webdriver.chrome.driver"] = chromedriver
         driver = webdriver.Chrome(chromedriver)
         #driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true', '--ssl-protocol=TLSv1'])
+        
         return driver
--- scrapper/coles/spiders/test/tests.py
+++ scrapper/coles/spiders/test/tests.py
@@ -1,11 +1,12 @@
 # encoding: utf-8
 import unittest
 import os
+from mock import *
 from scrapper.coles.spiders.coles import ColesSpider
 from scrapy.settings import Settings
 from mocked_data import coles_response, get_product_details
 from scrapper.coles.items import Product, Region
-
+from selenium import webdriver
 class TestColesSpider(unittest.TestCase):
 
     def setUp(self):
@@ -17,7 +18,7 @@ class TestColesSpider(unittest.TestCase):
     def test_get_drinks_homepages_urls(self):
         coles = ColesSpider(url='http://shop.coles.com.au/online/national/drinks')
         response = coles_response('fixtures/drinks.html')
-        urls = coles.get_homepages_url(response)
+        urls = coles._get_homepages_url(response)
         self.assertEquals(len(urls), 10)
         self.assertEquals(urls[0], u'http://shop.coles.com.au/online/national/drinks/soft-drinks-3314551')
         self.assertEquals(urls[1], u'http://shop.coles.com.au/online/national/drinks/cordials')
@@ -32,23 +33,16 @@ class TestColesSpider(unittest.TestCase):
 
     def test_get_search_url(self):
         coles = ColesSpider(url='http://shop.coles.com.au')
-        search_url = coles.generate_search_url(1,2,3,4)
         response = coles_response('fixtures/drinks_list.html')
-        url = coles.get_search_url(response)
+        url = coles._get_search_url(response)
         expected_url = "https://shop.coles.com.au/online/national/drinks/ColesCategoryView?orderBy=10601_6&productView=list&beginIndex=0&pageSize=20&catalogId=10576&storeId=10601&categoryId=3314551&localisationState=2&serviceId=ColesCategoryView&langId=-1"
         self.assertEquals(url, expected_url)    
-
-    def test_generate_search_url(self):
-        coles = ColesSpider(url='http://shop.coles.com.au')
-        search_url = coles.generate_search_url(1,2,3,4)
-        expected_url = u'https://shop.coles.com.au/online/national/drinks/ColesCategoryView?orderBy=1&productView=list&beginIndex=2&pageSize=20&catalogId=3&storeId=10601&categoryId=4&localisationState=2&serviceId=ColesCategoryView&langId=-1'
-        self.assertEquals(search_url, expected_url)
 
     def test_extract_search_parameters(self):
         coles = ColesSpider(url='http://shop.coles.com.au')
         search_dirty_parameters = u"{refreshId:'searchView', productView:'list', orderBy:'10601_6', beginIndex:'0', pageSize:'20', storeId:'10601', catalogId:'10576', categoryId:'3314551',topcategoryId:'',browseView:'true'}"
         
-        order_by, begin_index, catalogId, categoryId = coles.extract_search_parameters(search_dirty_parameters)
+        order_by, begin_index, catalogId, categoryId = coles._extract_search_parameters(search_dirty_parameters)
 
         self.assertEquals(order_by, '10601_6')
         self.assertEquals(begin_index, '0')
@@ -63,7 +57,7 @@ class TestColesSpider(unittest.TestCase):
         self.assertEquals(len(products_url), 20)
         self.assertEquals(products_url[0], u'http://shop.coles.com.au/online/national/coca-cola-coke-zero-soft-drink-bottle')
         self.assertEquals(products_url[1], u'http://shop.coles.com.au/online/national/coca-cola-diet-coke')
-        self.assertTrue("beginIndex=220" in search_url)
+        self.assertTrue("beginIndex=200" in search_url)
 
 
     def test_get_products_without_next_page(self):
@@ -82,9 +76,9 @@ class TestColesSpider(unittest.TestCase):
                 lon='144.98116', lat='-37.872868', service_type='HD', 
                 postcode='3182', webstore_id='0645', id='14854')
         coles.regions = [] 
-        product = coles.get_product_detail(response)
-        expected_info = {u'Contains Barley': u'Allergen:', u'Malted barley, hops, yeast and water.': u'Ingredients:', u'20': u'Size:', u'375ml': u'Serving Size:', u'1.0': u'Servings Per Pack:', u'* Percentage Daily Intake per serving. Percentage Daily Intakes are based on an average adult diet of 8700 kJ. Your daily intakes may be higher or lower depending on your energy needs.': u'Retail Limit:'}
-
+        product = coles._get_product_detail(response)
+        expected_info = {u'Retail Limit:': u'* Percentage Daily Intake per serving. Percentage Daily Intakes are based on an average adult diet of 8700 kJ. Your daily intakes may be higher or lower depending on your energy needs.', u'Allergen:': u'Contains Barley', u'Serving Size:': u'375ml', u'Size:': u'20', u'Ingredients:': u'Malted barley, hops, yeast and water.', u'Servings Per Pack:': u'1.0'}
+        
         self.assertEquals(product['name'], u'Birell Ultra Light Beer')
         self.assertEquals(product["brand"], u'Birell')
         self.assertEquals(product["general_price"], u'8.25')
@@ -99,31 +93,41 @@ class TestColesSpider(unittest.TestCase):
                 suburb='ST KILDA SOUTH', country='AU', zone_id= '0645HD',
                 lon='144.98116', lat='-37.872868', service_type='HD', 
                 postcode='3182', webstore_id='0645', id='14854')
-        product = coles.get_product_price_region(region, product)
-        product = coles.get_product_price_region(region, product)
+
+        coles._get_driver = Mock()        
+        coles._get_price_region = Mock(return_value='$14.00')        
+        product = coles._get_product_price_region(region, product)
+        
+        product = coles._get_product_price_region(region, product)
         
         self.assertEquals(product['id'], 84624)
         self.assertFalse(product['has_price_difference'])
         self.assertEquals(product['prices_region'][0]['postcode'], '3182')
-        self.assertEquals(product['prices_region'][0]['postcode'], '3182')
-        self.assertEquals(product['prices_region'][1]['price'], '$14.00')
+        self.assertEquals(product['prices_region'][1]['postcode'], '3182')
+        self.assertEquals(product['prices_region'][0]['price'], '$14.00')
         self.assertEquals(product['prices_region'][1]['price'], '$14.00')
     
-    def test_get_product_price_per_region(self):
+    def test_get_product_price_different_per_region(self):
         coles = ColesSpider(url='http://shop.coles.com.au/online/national/drinks')
         product = Product(id=84624, general_price=u'10.00', has_price_difference=False, redirect_url = 'http://shop.coles.com.au/webapp/wcs/stores/servlet/national/pepsi-max-soft-drink-cola-375ml-cans-7366022p', prices_region= [])
         region = Region(state='VIC', score='8.029414', collectionpoint='',
                 suburb='ST KILDA SOUTH', country='AU', zone_id= '0645HD',
                 lon='144.98116', lat='-37.872868', service_type='HD', 
                 postcode='3182', webstore_id='0645', id='14854')
-        product = coles.get_product_price_region(region, product)
-        product = coles.get_product_price_region(region, product)
+        
+        coles._get_driver = Mock()        
+        coles._get_price_region = Mock(return_value='$14.00')        
+        product = coles._get_product_price_region(region, product)
+        
+        coles._get_price_region = Mock(return_value='$24.00')        
+        product = coles._get_product_price_region(region, product)
         
         self.assertEquals(product['id'], 84624)
+        self.assertFalse(product['has_price_difference'])
         self.assertEquals(product['prices_region'][0]['postcode'], '3182')
-        self.assertEquals(product['prices_region'][0]['postcode'], '3182')
-        self.assertEquals(product['prices_region'][1]['price'], '$14.00')
-        self.assertEquals(product['prices_region'][1]['price'], '$14.00')
+        self.assertEquals(product['prices_region'][1]['postcode'], '3182')
+        self.assertEquals(product['prices_region'][0]['price'], '$14.00')
+        self.assertEquals(product['prices_region'][1]['price'], '$24.00')
 
     def test_generate_price_region_url(self):
         coles = ColesSpider(url='http://shop.coles.com.au/online/national/drinks')
@@ -132,7 +136,7 @@ class TestColesSpider(unittest.TestCase):
                 suburb='ST KILDA SOUTH', country='AU', zone_id= '0645HD',
                 lon='144.98116', lat='-37.872868', service_type='HD', 
                 postcode='3182', webstore_id='0645', id='14854')
-        price_region_url = coles.get_price_per_region_url(region, product)
+        price_region_url = coles._get_price_per_region_url(region, product)
         expected_url = u'https://shop.coles.com.au/online/COLLocalisationControllerCmd?redirectUrl=http://shop.coles.com.au/webapp/wcs/stores/servlet/national/pepsi-max-soft-drink-cola-375ml-cans-7366022p&suburbPostCodeId=14854&serviceType=HD&externalffmcId=0645&state=VIC&score=8.029414&collectionpoint=&suburb=ST KILDA SOUTH&zoneid=0645HD&postcode=3182&longitude=144.98116&latitude=-37.872868&storeId=10601'
         
         self.assertEquals(price_region_url, expected_url)
@@ -140,7 +144,7 @@ class TestColesSpider(unittest.TestCase):
     def test_get_region(self):
         coles_region = ColesSpider(postcodes= '3182')
         response = coles_response('fixtures/store-regions.json')
-        coles_region.get_region(response)
+        coles_region._get_region(response)
 
         self.assertEquals(coles_region.regions[0]['zone_id'], u'0645HD')
         self.assertEquals(coles_region.regions[0]['postcode'], u'3182')
